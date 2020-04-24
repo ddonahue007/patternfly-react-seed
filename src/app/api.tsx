9c77343391bc193825c280ac2fb325914caee787
Doug Donahue <ddonahue@redhat.com>
2020-04-24 15:21:46 -0400

    changed name of host and port vars

--- src/app/api.tsx
+++ src/app/api.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 // TODO: add to logger
-console.log('API HOST: %s', process.env.CUSTOMER_DEMO_API_SERVICE_HOST)
-console.log('API PORT: %s', process.env.CUSTOMER_DEMO_API_SERVICE_PORT)
+console.log('API HOST: %s', process.env.API_HOST)
+console.log('API PORT: %s', process.env.API_PORT)
 
 export default axios.create({
-  baseURL: `http://${process.env.CUSTOMER_DEMO_API_SERVICE_HOST}:${process.env.CUSTOMER_DEMO_API_SERVICE_PORT}/api`
+  baseURL: `http://${process.env.API_HOST}:${process.env.API_PORT}/api`
 });
