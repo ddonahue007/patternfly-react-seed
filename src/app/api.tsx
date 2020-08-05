6ceacfa308aaba7ecc88b305bbf2eeee422d0616
Doug Donahue <ddonahue@redhat.com>
2020-08-05 09:53:52 -0400

    fixed urls I think

--- src/app/api.tsx
+++ src/app/api.tsx
@@ -5,12 +5,13 @@ console.log('API HOST: %s', process.env.API_HOST)
 console.log('API PORT: %s', process.env.API_PORT)
 
 if (process.env.API_PORT) {
-  console.log('API URL', `http://${process.env.API_HOST}:${process.env.API_PORT}/api`);
-  process.env.API_URL=`http://${process.env.API_HOST}:${process.env.API_PORT}/api`
+  console.log('API URL = ', 'http://', `${process.env.API_HOST}`, ":", `${process.env.API_PORT}`, '/api');
+  process.env.API_URL="http://" + `${process.env.API_HOST}` + ":" + `${process.env.API_PORT}` + "/api"
 }
 else {
+  console.log('API URL = ', 'http://', `${process.env.API_HOST}`, '/api');
   console.log('API URL', `http://${process.env.API_HOST}/api`);
-  process.env.API_URL=`http://${process.env.API_HOST}/api`
+  process.env.API_URL="http://" + `${process.env.API_HOST}` + "/api"
 }
 
 export default axios.create({
